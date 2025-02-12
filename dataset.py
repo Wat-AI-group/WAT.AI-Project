46094b3d26bf661ad79de14a3df19cc8a9a82b08
Hari <harisherith@gmail.com>
2025-02-12 00:34:42 -0500

    O

--- dataset.py
+++ dataset.py
@@ -5,3 +5,10 @@ ds = load_dataset("JacobLinCool/VoiceBank-DEMAND-16k")
 
 # Check the dataset structure
 print(ds)
+
+sample = ds["train"][0]
+
+clean_audio = sample["clean"]
+noisy_audio = sample["noisy"]
+
+print(clean_audio)
